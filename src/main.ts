00edeb7a377a40a19e49500b77bc4bc0be318687
Anh Tuấn Hoàng <macos@TuanAnhs-Macbook.local>
2022-03-01 21:37:11 +0700

    codebase

---
+++ src/main.ts
@@ -0,0 +1,36 @@
+import { ValidationPipe, VersioningType } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { join } from 'path';
+import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
+// import { HttpExceptionFilter } from './errors/http-exception.filter';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.enableCors({
+    credentials: true,
+    exposedHeaders: ['Content-Disposition'],
+  });
+  app.setGlobalPrefix('/api');
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.use(cookieParser());
+  app.useGlobalPipes(new ValidationPipe({}));
+  // app.useGlobalFilters(new HttpExceptionFilter());
+
+  const config = new DocumentBuilder()
+    .setTitle('Building`s Api')
+    .setDescription('The building API description')
+    .setVersion('1.0')
+    .addBearerAuth({ in: 'header', type: 'http' })
+    .addTag('Building')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.PORT);
+  console.log('Listening at PORT: ' + process.env.PORT);
+}
+bootstrap();
